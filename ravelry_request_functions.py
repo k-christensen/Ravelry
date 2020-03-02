d336a40ad6b46dc60d26ade7d2338b8cbbe3be93
Kate Christensen <katec125@gmail.com>
2020-03-01 23:18:29 -0600

    "edited notes for get favs list"

--- ravelry_request_functions.py
+++ ravelry_request_functions.py
@@ -8,30 +8,23 @@ import pdb
 from bs4 import BeautifulSoup
 import personal_keys
 
+
+# get favs list: input is username, output is list of pattern ids in a person's favorites
+# if the user has over 500 favorites, then only returns the first 500
+# this is done so other functions don't take forever
 def get_favs_list(username):
     fav_list = []
     favs_url = 'https://api.ravelry.com/people/{}/favorites/list.json'.format(username)
-#     following request returns json with two keys: favorites and paginator
-#     for the following request, I've added the params page size, 
-#     which is 100 (maximum size), and page number (first page in the following request)
     favs = requests.get(favs_url, auth = (personal_keys.username(),personal_keys.password()),
                         params={'page_size':100, 'page':1})
-#     the following will add a list of all the pattern ids for the favorited patterns and turns them into a list, which is then appended to the fav_list defined at the beginning of the function
     for item in range(0,len(favs.json()['favorites'])):
         if favs.json()['favorites'][item]['favorited'] is not None:
             if 'pattern_id' in favs.json()['favorites'][item]['favorited'].keys():
                 fav_list.append(favs.json()['favorites'][item]['favorited']['pattern_id'])
             elif 'id' in favs.json()['favorites'][item]['favorited'].keys():
                 fav_list.append(favs.json()['favorites'][item]['favorited']['id'])
-
-#     in the event the user has more than 100 favorites, 
-# this loop will essentially go in and make a new request for the next page of likes, 
-# this loop finishes when the page number (which starts at 2 because we've already requested page one above) 
-# equals the last page
     if favs.json()['paginator']['page_count']>1:
-#         page_number is 2 because first page is already in fav_list
         page_number = 2
-#         loop to do successive requests for new pages of favorited pattern ids
         if favs.json()['paginator']['last_page'] > 5:
             last_page = 5
         else:
@@ -39,16 +32,13 @@ def get_favs_list(username):
         while page_number <= last_page:
             new_request_favs = requests.get(favs_url, auth = (personal_keys.username(),personal_keys.password()),
                         params={'page_size':100, 'page':page_number})
-#             append the new list of favorites to the old list containing the first 100 favorites
             for item in range(0,len(new_request_favs.json()['favorites'])):
                 if new_request_favs.json()['favorites'][item]['favorited'] is not None:
                     if 'pattern_id' in new_request_favs.json()['favorites'][item]['favorited'].keys():
                         fav_list.append(new_request_favs.json()['favorites'][item]['favorited']['pattern_id'])
                     elif 'id' in new_request_favs.json()['favorites'][item]['favorited'].keys():
                         fav_list.append(new_request_favs.json()['favorites'][item]['favorited']['id'])
-#             add one to the page number so it will request the page following page 2, this will be done for as long as there are more pages to be requested
             page_number += 1
-#     since the fav_list is currently a list of lists, the following function flattens the fav_list into one long list of ids
     return fav_list
 
 def get_friend_favs(username):    
