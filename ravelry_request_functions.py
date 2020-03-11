67609f78d330e19fb58f8214d461e6e16b4aa6cb
Kate Christensen <katec125@gmail.com>
2020-03-11 18:09:53 -0500

    "broke up search function into two functions"

--- ravelry_request_functions.py
+++ ravelry_request_functions.py
@@ -8,11 +8,15 @@ import pdb
 from bs4 import BeautifulSoup
 import personal_keys
 
-# search term goes in, list of pattern ids comes out
+# search term goes in, search json comes out
 def search(term):
     search_url = 'https://api.ravelry.com/patterns/search.json?query={}'.format(term)
     search = requests.get(search_url, auth = (personal_keys.username(),personal_keys.password()))
     s_json = search.json()
+    return s_json
+
+# json file of patterns goes in, list of pattern ids comes out
+def search_pattern_list(s_json):
     p_list = s_json['patterns']
     return [p_list[ind]['id'] for ind in list(range(0,len(p_list)))]
 
