b514af26db47934a677cf4928086b004fd14312a
Kate Christensen <katec125@gmail.com>
2020-03-22 16:39:08 -0500

    "split fav funcs into separate friend funcs"

--- python functions/fav_funcs.py
+++ python functions/fav_funcs.py
@@ -45,21 +45,3 @@ def get_favs_list(username):
             fav_list.extend(create_fav_list(new_request_favs))
             page_number += 1
     return fav_list
-
-# input: username, output: list of user's friends
-def friend_username_list(username):
-    user_url = 'https://api.ravelry.com/people/{}/friends/list.json'.format(username)
-    user = requests.get(user_url, 
-                        auth = (personal_keys.username(),personal_keys.password()))
-    return [user.json()['friendships'][item]['friend_username'] 
-            for item in range(0,len(user.json()['friendships']))]
-
-# input: username, output: user's friends' favs 
-def get_friend_favs(username):    
-    friend_list = friend_username_list(username)
-    all_friend_projs = []
-    for user in friend_list:
-        all_friend_projs.append(get_favs_list(user))   
-    flat_list = [item for sublist in all_friend_projs for item in sublist]
-    edited_flat_list = [item for item in flat_list if item is not None]
-    return edited_flat_list
---
+++ python functions/friend_funcs.py
@@ -0,0 +1,28 @@
+import requests
+import time
+import pandas as pd
+import numpy as np
+import re
+import json
+import pdb
+from bs4 import BeautifulSoup
+import personal_keys
+from fav_funcs import *
+
+# input: username, output: list of user's friends
+def friend_username_list(username):
+    user_url = 'https://api.ravelry.com/people/{}/friends/list.json'.format(username)
+    user = requests.get(user_url, 
+                        auth = (personal_keys.username(),personal_keys.password()))
+    return [user.json()['friendships'][item]['friend_username'] 
+            for item in range(0,len(user.json()['friendships']))]
+
+# input: username, output: user's friends' favs 
+def get_friend_favs(username):    
+    friend_list = friend_username_list(username)
+    all_friend_projs = []
+    for user in friend_list:
+        all_friend_projs.append(get_favs_list(user))   
+    flat_list = [item for sublist in all_friend_projs for item in sublist]
+    edited_flat_list = [item for item in flat_list if item is not None]
+    return edited_flat_list
