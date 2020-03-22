81d0e5b51acb169eb99b632ec97cea1e2e07f651
Kate Christensen <katec125@gmail.com>
2020-03-22 16:18:42 -0500

    "fav funcs now also has friends"

--- python functions/fav_funcs.py
+++ python functions/fav_funcs.py
@@ -31,3 +31,21 @@ def create_fav_list(fav_request):
 def fav_req_to_list(username, page_size, page):
     fav_request = fav_request(username, page_size, page)
     return create_fav_list(fav_request)
+
+# input: username, output: list of user's friends
+def friend_username_list(username):
+    user_url = 'https://api.ravelry.com/people/{}/friends/list.json'.format(username)
+    user = requests.get(user_url, 
+                        auth = (personal_keys.username(),personal_keys.password()))
+    return [user.json()['friendships'][item]['friend_username'] 
+            for item in range(0,len(user.json()['friendships']))]
+
+# input: username, output: user's friends' favs 
+def get_friend_favs(username):    
+    friend_list = friend_username_list(username)
+    all_friend_projs = []
+    for user in friend_list:
+        all_friend_projs.append(get_favs_list(user))   
+    flat_list = [item for sublist in all_friend_projs for item in sublist]
+    edited_flat_list = [item for item in flat_list if item is not None]
+    return edited_flat_list
