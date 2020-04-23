b0b1b79e8c3c175e0595bf9724c4ef2d36b83358
Kate Christensen <katec125@gmail.com>
2020-04-23 03:18:27 -0500

    working on full search function

--- notebooks/python_functions/search_functions.py
+++ notebooks/python_functions/search_functions.py
@@ -24,15 +24,60 @@ def search_to_list(term):
     s_json = search(term)
     return search_pattern_list(s_json)
 
+# turns list into string separated by '%7C', which is 'or' in the rav search url
+def or_string(attr_list):
+    return '%7C'.join(attr_list)
+
+# creates the unique part of the search url
+def unique_search_url_section(attr_dict):
+    yarn_str = or_string(create_yarn_list(attr_dict['yarn_weight']))
+    attr_str = or_string(attr_dict['pattern_attributes'])
+    cat_str = or_string(attr_dict['pattern_categories'][1:])
+    return 'weight={}&pa={}&pc={}'.format(yarn_str,attr_str,cat_str)
+
+# creates full search url
+def full_search_url(url_sect):
+    return 'https://api.ravelry.com/patterns/search.json?{}&sort=recently-popular&view=captioned_thumbs'.format(url_sect)
+
+# combo of previous two functions
+def create_search_url(attr_dict):
+    url_sect = unique_search_url_section(attr_dict)
+    return full_search_url(url_sect)
+
+
+
+
 # testing work~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-search_url = 'https://api.ravelry.com/patterns/search.json?pc=pullover&sort=recently-popular&view=captioned_thumbs'
+search_url = create_search_url(attr_dict)
 search = requests.get(search_url, auth = (personal_keys.username(),personal_keys.password()))
 s_json = search.json()
-s_json['patterns'][5]['name']
+search
 
+create_search_url(attr_dict)
 
 search_example = search("#pc=pullover&sort=recently-popular&view=captioned_thumbs")
 search_example['patterns'][0]['name']
+
+from fav_funcs import *
+from proj_funcs import *
+from pattern_attr_funcs import *
+from yarn_weights import *
+
+attr_dict = single_request_to_attrs('36436')
+
+attr_dict
+
+'%7C'.join(create_yarn_list(attr_dict['yarn_weight']))
+'%7C'.join(attr_dict['pattern_attributes'])
+
+attr_dict['pattern_categories'][1:]
+
+
+yarn_str = or_string(create_yarn_list(attr_dict['yarn_weight']))
+attr_str = or_string(attr_dict['pattern_attributes'])
+cat_str = or_string(attr_dict['pattern_categories'][1:])
+'weight={}&pc={}&pa={}'.format(yarn_str,attr_str,cat_str)
+
 
 # would want to do some kind of loop like "%7C" with the different attributes, 
 # weight=[weight-1{}weight{}weight+1]&pc=[pattern_cat]&pa = [pattern_attr{}pattern_attr]
