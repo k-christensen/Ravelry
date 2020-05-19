9a713872a985e92fff8db99004eee6d0b5377cf6
Kate Christensen <katec125@gmail.com>
2020-05-18 20:00:21 -0500

    completed some of it, have notes for rest of it

--- notebooks/python_functions/all_components.py
+++ notebooks/python_functions/all_components.py
@@ -13,6 +13,7 @@ from proj_funcs import *
 from pattern_attr_funcs import *
 from search_functions import *
 from yarn_weights import create_yarn_list
+from count_df_funcs import *
 
 
 username = 'katec125'
@@ -22,6 +23,16 @@ user_profile = user_profile(username)
 
 known_pattern_list = known_pattern_list(username)
 
-pattern_pool_json(search)
+search_list = pattern_pool_list(search)
 
-create_pattern_id_list()
+search_minus_knowns =   [item for item in search_list 
+                        if item not in known_pattern_list]
+
+pattern_pool = multiple_pattern_request(search_minus_knowns)
+
+pool_idf = #maybe just make this a list object
+
+predicted_user_prefs = #dictionary with ids and predicted like scores
+
+# updata pattern pool to include predicted user prefs, 
+# maybe order the entries by the predicted user prefs?
